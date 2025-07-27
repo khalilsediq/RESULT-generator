dfc710ff4e30165c2371a142ee831f556cadcbc5
khalilSediq <khalilullahsadique1334@gmail.com>
2025-07-27 18:09:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// var fullName = document.querySelector("#fullName")
+// var rollNumber = document.querySelector("#rollNumber");
+// var sub1 = document.querySelector("#sub1");
+// var sub2 = document.querySelector("#sub2");
+// var sub3 = document.querySelector("#sub3");
+// var resul = document.querySelector("#result");
+
+// function resultGenerator() {
+//     var totaMarks = 400;
+//     var obtainedMarks = +sub1.value + +sub2.value + +sub3.value;
+//     var percentage = obtainedMarks / totaMarks * 100;
+//     console.log(percentage);
+    
+//     var finalmessage = fullName.value + " with roll number  (" + rollNumber.value + ") has obtained" + obtainedMarks + " marks out of " + totaMarks + "and percentage is " + percentage + "%";
+//     resul.innerHTML = finalmessage;
+// }
+
+
+
+let fruits = [
+    "apple", "mango ", "banana", "melon"
+]
+// fruits.push("pineapple")
+fruits.unshift("khan")
+// fruits.shift("")
+// fruits.pop()
+console.log(fruits);
